ec0d1350d7da2f2dda3c0577f3be8f2be2116876
sohidul8305 <si616132@gmail.com>
2025-08-15 10:19:01 +0600

    insert solution return and consol er modda parthokko

---
+++ task02.js
@@ -0,0 +1,17 @@
+function sum(input1, input2) {
+    let result = input1 + input2;
+
+    return result;
+}
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+
+    return (result);
+}
+
+let a = 4;
+let b = 5;
+
+let finalresult = sum(a, b) + multiply(a, b);
+console.log(finalresult);
