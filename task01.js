18444afb265aa90dcf704d22a984d4646f4b80bd
sohidul8305 <si616132@gmail.com>
2025-08-15 09:59:54 +0600

    javascript basic solution problem

---
+++ task01.js
@@ -0,0 +1,14 @@
+const numbers = [15, 30, 85, 60];
+const min = numbers[0];
+if (min < numbers) {
+    min = numbers
+}
+function allnumber(number) {
+    for (const num of number) {
+
+    }
+    return min;
+}
+
+const reguler = allnumber(numbers);
+console.log(reguler);
